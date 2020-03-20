c5debebb2c3bacb7a1da936c75ceffa57ecfc79d
Percy <percyperezdante@gmail.com>
2020-03-20 19:57:32 +0000

    Update path to root for virtual box additional

--- oracle_linux_6/scripts/virtualbox.sh
+++ oracle_linux_6/scripts/virtualbox.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
+yum install -y gcc kernel-uek-devel
+
+set -x
+
 echo "Installing the virtualbox guest additions"
-VBOX_VERSION=$(cat /home/ajayu/.vbox_version)
+VBOX_VERSION=$(cat /root/.vbox_version)
 cd /tmp || exit 1
 echo "Mounting VBoxGuestAdditions iso..."
-mount -o loop /home/ajayu/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /root/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 echo "Installing VBoxLinuxAdditions"
 sh /mnt/VBoxLinuxAdditions.run
 sleep 40
 umount /mnt
 sleep 40
-rm -rf /home/ajayu/VBoxGuestAdditions_*.iso
+rm -rf /root/VBoxGuestAdditions_*.iso
 sleep 40
